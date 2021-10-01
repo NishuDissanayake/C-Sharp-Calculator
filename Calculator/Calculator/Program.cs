1a74629169daa3975ef14e7ee6f2993920788046
NishuDissanayake <nishudissanayake5252@gmail.com>
2021-10-02 00:15:01 +0530

    project added

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first value: ");
+            int no1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second value: ");
+            int no2 = int.Parse(Console.ReadLine());
+
+            
+            Console.ReadLine();
+        }
+    }
+}
